a0c18dd6c95dc0322c598b83dd35125dc96cddae
DESKTOP-0BM11R7\yulmi <yulmi123@naver.com>
2023-03-03 17:38:34 +0900

    eeeeeeerorr partyyyyyyy

--- src/main/java/com/bbang/shuttle/MybatisConfig.java
+++ src/main/java/com/bbang/shuttle/MybatisConfig.java
@@ -1,7 +1,5 @@
 package com.bbang.shuttle;
 
-import lombok.RequiredArgsConstructor;
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -15,32 +13,20 @@ import javax.sql.DataSource;
 
 // application.properties 설정 대신 좀더 상세한 설정을 위한 config
 @Configuration
-@RequiredArgsConstructor
-@MapperScan(basePackages = "com.bbang.shuttle.mapperInterface")
+@MapperScan("com.example.mapper")
 public class MybatisConfig {
 
   @Autowired
   private DataSource dataSource;
 
   @Bean
-  public DataSource dataSource() {
-    // 데이터베이스 설정
-    BasicDataSource dataSource = new BasicDataSource();
-    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-    dataSource.setUrl("jdbc:mysql://localhost:3306/mydb?useSSL=false&serverTimezone=Asia/Seoul");
-    dataSource.setUsername("root");
-    dataSource.setPassword("password");
-
-    return dataSource;
-  }
-
-  @Bean
   public SqlSessionFactory sqlSessionFactory() throws Exception {
     SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
     sessionFactory.setDataSource(dataSource);
 
-    PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-    sessionFactory.setMapperLocations(resolver.getResources("classpath:/mapper/*.xml"));
+    // mybatis-config.xml 파일 위치를 설정해줍니다.
+    sessionFactory.setConfigLocation(new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml"));
+
     return sessionFactory.getObject();
   }
 
@@ -48,5 +34,4 @@ public class MybatisConfig {
   public SqlSessionTemplate sqlSession() throws Exception {
     return new SqlSessionTemplate(sqlSessionFactory());
   }
-
 }
